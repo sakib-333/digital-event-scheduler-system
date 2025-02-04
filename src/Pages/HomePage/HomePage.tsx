0dc068b12876f5312afe1733bd1c1199aac9c61e
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-02-04 16:22:21 +0600

    Add react-router-dom.

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-sky-300">Hello React</h1>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+};
+
+export default HomePage;
