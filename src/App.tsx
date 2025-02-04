e3629cd9a66929c781cc18b3f1fe84f25dec8fa4
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-02-04 16:12:23 +0600

    Add material ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Button from "@mui/material/Button";
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold bg-sky-300">Hello React</h1>
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 };
